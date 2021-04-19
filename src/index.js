b6af0f786186f4b656273d1e56fce3ea5368af81
pamela828 <pamelaespinosaj8@gmail.com>
2021-04-18 22:03:47 -0500

    Added weather api

---
+++ src/index.js
@@ -0,0 +1,49 @@
+let apiKey = "bded79ed1d2cea9265b0acc8da5369f4";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+
+function formatDate(timestamp) {
+let date = new Date(timestamp);
+let hours = date.getHours();
+if (hours <10) {
+    hours = `0${hours}`;
+}
+let minutes = date.getMinutes();
+if (minutes <10) {
+    minutes = `0${minutes}`;
+}
+let days = [
+    "Sunday", 
+    "Monday", 
+    "Tuesday", 
+    "Wednesday", 
+    "Thursday", 
+    "Friday", 
+    "Saturday"
+];
+let day = days[date.getDay()];
+return `${day} ${hours}:${minutes}`;
+}
+
+function displayTemperature(response) {
+console.log(response.data);
+let temperatureElement = document.querySelector("#temperature");
+temperatureElement.innerHTML = Math.round(response.data.main.temp);
+
+let cityElement = document.querySelector("#city")
+cityElement.innerHTML = response.data.name;
+
+let descriptionElement = document.querySelector("#description");
+descriptionElement.innerHTML = response.data.weather[0].description;
+
+let humidityElement = document.querySelector("#humidity");
+humidityElement.innerHTML= response.data.main.humidity;
+
+let windElement = document.querySelector("#wind");
+windElement.innerHTML = Math.round(response.data.wind.speed);
+
+let dateElement = document.querySelector("#date");
+dateElement.innerHTML = formatDate(response.data.dt * 1000);
+}
+
+console.log(apiUrl);
+axios.get(apiUrl).then(displayTemperature);
